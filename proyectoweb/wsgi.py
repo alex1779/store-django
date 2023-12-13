5c6b1c5014ab188cc842811f0d613bb73872d7a9
alex1779 <alex1779@gmail.com>
2023-12-13 18:13:31 -0300

    update

--- proyectoweb/wsgi.py
+++ proyectoweb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoWeb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoweb.settings')
 
 application = get_wsgi_application()
