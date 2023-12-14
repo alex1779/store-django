50ee47b0e89592ba4b0a1572ec0b38554921cbfa
alex1779 <alex1779@gmail.com>
2023-12-14 17:47:45 -0300

    update

--- proyectoweb/settings.py
+++ proyectoweb/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
