748e7d581b946d3020b52463c8e94e901d4610a6
alex1779 <alex1779@gmail.com>
2023-12-27 21:27:32 -0300

    Update

--- proyectoweb/settings.py
+++ proyectoweb/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'proyectowebapp/')
 
-STATIC_URL = '/proyectowebapp/'
+STATIC_URL = '/alejandro/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
