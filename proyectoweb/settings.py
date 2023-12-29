923fb5e375c01ae430687d881b3b9168fb6aeb71
alex1779 <alex1779@gmail.com>
2023-12-29 16:56:57 -0300

    Update

--- proyectoweb/settings.py
+++ proyectoweb/settings.py
@@ -115,13 +115,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'proyectowebapp/static')
 
 STATIC_URL = 'static'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
+STATICFILES_DIRS = []
 
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:
