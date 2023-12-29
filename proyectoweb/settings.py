438cb6753cd95e7a31cf497d72bc90843ad3d626
alex1779 <alex1779@gmail.com>
2023-12-29 20:41:14 -0300

    update

--- proyectoweb/settings.py
+++ proyectoweb/settings.py
@@ -115,11 +115,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = []
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:
--- proyectowebapp/apps.py
+++ proyectowebapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProyectowebappConfig(AppConfig):
+class proyectowebappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'proyectowebapp'
