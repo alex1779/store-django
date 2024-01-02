8babc3cabdf033281c64412e11f0037de3574ffb
alex1779 <alex1779@gmail.com>
2024-01-02 17:38:08 -0300

    update

--- proyectoweb/settings.py
+++ proyectoweb/settings.py
@@ -155,8 +155,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "alex1779@gmail.com"
-EMAIL_HOST_PASSWORD = "BuloneriaMecfa2507"
+EMAIL_HOST_USER = "pruebastest577@gmail.com"
+EMAIL_HOST_PASSWORD = "Oreja*14420"
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
